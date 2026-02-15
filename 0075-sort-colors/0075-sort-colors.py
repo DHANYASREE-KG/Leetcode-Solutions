973950e614cc832413ac8684642c6393c78fea0f
DHANYASREE-KG <dhanyakathir2005@gmail.com>
2026-02-15 21:24:25 +0530

    Time: 7 ms (6.31%), Space: 12.6 MB (19.53%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,9 +2,7 @@ class Solution(object):
     def sortColors(self, nums):
         n=len(nums)
         for i in range(n):
-            a=i
-            for j in range(i+1,n):
-                if nums[j]<nums[a]:
-                    a=j
-            nums[i],nums[a]=nums[a],nums[i]
+            for j in range(0,n-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
         return nums
