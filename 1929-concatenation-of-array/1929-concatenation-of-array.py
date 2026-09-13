7be9fcec23a02c707f2fc45b474fba666f80cfc0
Dhanya Sree K G <dhanyakathir2005@gmail.com>
2026-09-13 13:18:00 +0530

    Time: 0 ms (100%), Space: 12.7 MB (35.43%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        n=[]
+        temp=nums
+        for i in nums:
+          n+=nums
+          break
+        for i in temp:
+          n+=nums
+          break
+        return n
+
+        
+        
