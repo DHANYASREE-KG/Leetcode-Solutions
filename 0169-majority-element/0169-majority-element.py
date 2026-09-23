614e054ea7e896b44fa10418b2454e3cd5ef0e81
Dhanya Sree K G <dhanyakathir2005@gmail.com>
2026-09-23 21:00:17 +0530

    Time: 22 ms (12.23%), Space: 13.6 MB (74.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,16 @@
 class Solution(object):
     def majorityElement(self, nums):
-        nums.sort()
-        return nums[len(nums)//2]
+        count={}
+        for i in nums:
+            if i in count:
+                count[i]+=1
+            else:
+                count[i]=1
+        item=list(count.items())
+        for i in range(len(item)):
+            for j in range(len(item)-i-1):
+                if item[j][1]<item[j+1][1]:
+                    item[j],item[j+1]=item[j+1],item[j]
+        return item[0][0]
        
         
